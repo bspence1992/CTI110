6fdfda5ae099181af36eeb3f8c56e67871bf7db5
Brandon <bspence1992@gmail.com>
2025-09-16 14:36:11 -0400

    Add files via upload

---
+++ P1LAB1_SpencerBrandon.py
@@ -0,0 +1,9 @@
+# Brandon Spencer
+# 09/16/2025
+# P1LAB1
+# User enters first ans last name and "Welcome to CTI-110"
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
+
